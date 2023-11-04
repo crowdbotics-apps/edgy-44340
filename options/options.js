d3714aa0b05437f4f333dcbac168b63a2b081666
Crowdbotics <team@crowdbotics.com>
2023-11-04 22:35:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "edgy-44340",
+  url: "https://edgy-44340.botics.co",
+  api: "https://edgy-44340.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
